11df7b88c5801d0ecaf8eeb6879fcd34baa8fe4b
Qusay Fantazia <Q.Fatazia@student.tudelft.nl>
2020-07-14 15:24:35 +0200

    Updates

---
+++ precache-manifest.80c3916ac5cd5093752f9b87597bda97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "666842cc17645333386c",
+    "url": "/Monster-Rolodex/static/css/main.2c1976e8.chunk.css"
+  },
+  {
+    "revision": "666842cc17645333386c",
+    "url": "/Monster-Rolodex/static/js/main.4acae309.chunk.js"
+  },
+  {
+    "revision": "1c53a097523ea440329e",
+    "url": "/Monster-Rolodex/static/js/runtime~main.5e73536d.js"
+  },
+  {
+    "revision": "73c74a9bb63118e4d3d0",
+    "url": "/Monster-Rolodex/static/js/2.e3a2b14b.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Monster-Rolodex/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ccfedf1df02119b8046e68f65ea9634c",
+    "url": "/Monster-Rolodex/index.html"
+  }
+];
